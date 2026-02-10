3d1c876f738d75bf26c712443f41efa732577333
VETRI-VETSAN <vetri@bocxy.com>
2026-02-10 17:32:50 +0530

    Initial commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,36 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject, Input, input, signal } from '@angular/core';
+import { InvestmentService } from '../service/investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  private investmentService =inject(InvestmentService);
+
+  results =computed(()=>this.investmentService.resultsData());
+  
+
+  // results=input<{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[]>();
+
+  // @Input() results?:{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+
+}
